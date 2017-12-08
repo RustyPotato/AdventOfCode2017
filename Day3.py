dfae7d4f88a44c5001ae8eb66e84f756dd543edc
RustyPotato <kylerfawcett@gmail.com>
2017-12-08 10:16:26 -0500

    Part 1 of day 3. Part 2 is listed online as a solution.

--- Day3.py
+++ Day3.py
@@ -1,31 +1,40 @@
 #---------------#
 #     DAY 3     #
 #---------------#
-target = 289326
-layer = 0
-for i in range(target): 
-    if (i%2 == 1):
-        if (i*i <= target):
-            layer = i
-        else:
-            break
-print("Layer", layer)
-print("Layer from", layer**2, "to", (layer+2)**2)
-
-print("Steps from start =", target - (layer**2 + 1))
-side = (target - (layer**2 + 1))//(layer+1)
-print("On side", side)
-pos = ((target - (layer**2))%(layer+1))
-print("Steps from side start", pos)
-
-deltaX = 0
-deltaY = 0
-
-if (target == layer**2):
-    print("Moves required asdf = ", 2*(layer-2))
-else:
-    deltaY = layer-1
-    deltaX = -(layer-1)+pos
+def day3Challenge(target):
+    layer = 0
+    for i in range(target): 
+        if (i%2 == 1):
+            if (i*i <= target):
+                layer = i
+            else:
+                break
+    side = ((target-((layer)**2)))//(layer+1)
+    pos = ((target - ((layer-2)**2)) % (layer+1))
+        
+    deltaX = 0
+    deltaY = 0
+    
+    if (target == layer**2):
+        deltaX = layer//2
+        deltaY = layer//2
+    else:
+        if (side == 0):
+            deltaX = ((layer+2)//2)
+            deltaY = ((layer+2)//2)-pos
+        if (side == 1):
+            deltaY = ((layer+2)//2)
+            deltaX = (-((layer+2)//2)+pos)
+        if (side == 2):
+            deltaY = ((layer+2)//2)
+            deltaX = -(-((layer+2)//2)+pos)
+        if (side == 3):
+            deltaX = ((layer+2)//2)
+            deltaY = -(-((layer+2)//2)+pos)
+            
+        
     print("dX = ", deltaX, "  dY = ", deltaY)
-    print("Moves required = ", abs(deltaX)+abs(deltaY))
-
+    print("Distance to origin =", abs(deltaX)+abs(deltaY))
+    return (deltaX, deltaY)
+    
+day3Challenge(289326)
