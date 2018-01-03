25bf0dfdbef4ee733d24ce6fbbeabe745125c0f0
RustyPotato <kylerfawcett@gmail.com>
2018-01-03 13:54:48 -0500

    Day 15 part 2 and readme update.

--- Day15.py
+++ Day15.py
@@ -10,13 +10,38 @@ for i in range(40000000):
     
     # Judging time
     if (aRes % 65536 == bRes % 65536):
+        print("Match found! With ", aRes, " and ", bRes, sep="")
+        judgeCounter += 1
+    
+    # Setup for next round
+    lastA = aRes
+    lastB = bRes
+print("Final judge score, part 1:", judgeCounter)
+
+
+## Part 2
+lastA = 116
+lastB = 299
+judgeCounter = 0
+
+for i in range(5000000):
+    # Generation cycle
+    aRes = (lastA * 16807) % 2147483647
+    while (aRes % 4 != 0):
+        aRes = (aRes * 16807) % 2147483647
+    bRes = (lastB * 48271) % 2147483647
+    while (bRes % 8 != 0):
+        bRes = (bRes * 48271) % 2147483647
+    
+    # Judging time
+    if (aRes % 65536 == bRes % 65536):
         print(i, ": Match found!", sep="")
         judgeCounter += 1
     
     # Setup for next round
     lastA = aRes
     lastB = bRes
-print("Final judge score:", judgeCounter)
+print("Final judge score, part 2:", judgeCounter)
 
 
 
